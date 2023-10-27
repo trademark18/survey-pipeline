b1bd78540b066f26da65d196e3e262532d838298
Jared Messer <jtech95@live.com>
2023-10-26 23:11:27 -0400

    Add user, add policy, display definitely unsafe output on CF stack

--- src/lib/ingest-construct.ts
+++ src/lib/ingest-construct.ts
@@ -3,6 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 interface IIngestParams {
   readonly ingestQueue: cdk.aws_sqs.Queue;
@@ -14,6 +15,41 @@ export class IngestConstruct extends Construct {
 
     // S3 Bucket
     const bucket = new s3.Bucket(this, 'ingestBucket', {});
+
+    //IAM User
+    const user = new iam.User(this, 'IngestUser');
+    const accessKey = new iam.AccessKey(this, 'AccessKey', { user });
+
+    const policy = new iam.Policy(this, 'IngestUploadPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['s3:PutObject'],
+          resources: [`${bucket.bucketArn}/*`],
+        }),
+      ],
+    });
+
+    user.attachInlinePolicy(policy);
+
+
+
+    // Output the Access Key ID and Secret Access Key
+    new cdk.CfnOutput(this, 'AccessKeyIdOutput', {
+      value: accessKey.accessKeyId,
+    });
+    new cdk.CfnOutput(this, 'SecretAccessKeyOutput', {
+      value: accessKey.secretAccessKey.unsafeUnwrap(),
+    });
+
+    // Output Ingest Bucket Information
+    new cdk.CfnOutput(this, 'IngestBucketEndpoint', {
+      value: bucket.bucketDomainName
+    })
+    new cdk.CfnOutput(this, 'IngestBucketName', {
+      value: bucket.bucketName
+    })
+
+    
 
     // Load S3 bucket with example files
     // new s3deploy.BucketDeployment(this, 'DeployFiles', {
